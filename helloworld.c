a6402d2bec5fa652779c320e7a29f3ee72331b8e
xm <xm@xmdeMacBook-Air.local>
2024-06-11 16:16:11 +0800

    test

--- helloworld.c
+++ helloworld.c
@@ -1,5 +1,20 @@
 #include<stdio.h>
 
+typedef struct {
+  char * name;
+  int age;
+}UserWriteVo;
+
+typedef struct {
+  char * name;
+  int age;
+}User;
+
 int main(){
+  User user = {"zhangxiaomin",12};
+  printf("%s %d\n",user.name,user.age);
+  UserWriteVo userWriteVo = {"zhangxiaomin",12};
+  printf("%s %d\n",userWriteVo.name,userWriteVo.age);
+  printf("%s %d\n",user.name,user.age);
   return 0;
 }

4fbd1154e8b13bf65e3d6de873383ac5f43266a0
lvlv6994 <lvlv6994@gmail.com>
2024-06-26 06:10:58 +0800

    test

--- helloworld.c
+++ helloworld.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
+#include<stdlib.h>
 
+
+typedef struct {
+  char * name;
+  int age;
+}UserListVo;
 
 typedef struct {
   char * name;
@@ -12,10 +18,15 @@ typedef struct {
 }User;
 
 int main(){
+  UserListVo userlistvo;
+  userlistvo.name = "zhangxiaomin";
+  userlistvo.age = 1;
+  printf("%s %d\n",userlistvo.name,userlistvo.age);
   User user = {"zhangxiaomin",12};
   printf("%s %d\n",user.name,user.age);
   UserWriteVo userWriteVo = {"zhangxiaomin",12};
   printf("%s %d\n",userWriteVo.name,userWriteVo.age);
   printf("%s %d\n",user.name,user.age);
+  exit(0);
   return 0;
 }
